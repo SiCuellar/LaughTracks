479666cfcebde759d1f6de05157afa3dc907dedd
SilvestreCuellar <cuellarsilvestre@gmail.com>
2018-10-03 22:53:10 -0600

    yay got my data base to populate using my seeding file

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -1,7 +1,17 @@
+require 'pry'
+
+require_relative '../models/comedian.rb'
+
+
 class LaughTracksApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
 
+  # get '/' do
+  #   erb :dashboard
+  # end
+
   get '/' do
-    'please work!'
+    @comedians = Comedian.all
+    erb :dashboard
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,24 @@
+require_relative '../app/models/comedian.rb'
 
+Comedian.destroy_all
+
+
+Comedian.create(name: 'Jim Carrey', age: 56, city: 'Newmarket, Canada')
+
+# Comedian.create!([{
+#   name: "Jim Carrey",
+#   age: 56,
+#   city: "Newmarket, Canada"}
+# ,
+#   {
+#   name: "Jerry Seinfield",
+#   age: 64,
+#   city: "Brooklyn, New York"},
+#
+# {
+#   name: "Joan Rivers",
+#   age: 81,
+#   city: "Brooklyn, New York"}
+# ])
+
+p "Created #{Comedian.count} Comedians"
